e03a845a99b8ad1e938f7d90d7b3410d55a8953d
João Rambo <jpramboguanabara@hotmail.com>
2022-09-21 21:32:46 -0300

    first commit interface and components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import { Participant } from "../../components/Participant"
+import { styles } from './styles'
+
+export function Home() {
+  function handleAddParticipant() {
+    console.log('added')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do Evento
+      </Text>
+      <Text style={styles.eventDate}
+      >
+        Sexta, 4 de Novembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={handleAddParticipant}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
+    </View>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#121212',
+    flex: 1,
+    padding: 24
+  },
+  eventDate: {
+    color: '#6B6B6B', 
+    fontSize: 16,
+  },
+  eventName: {
+    marginTop: 58,
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    height: 56,
+    flex: 1,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+
+    color: '#FFF',
+    padding: 16,
+    marginRight: 12,
+
+    fontSize: 16,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42
+  }
+})
